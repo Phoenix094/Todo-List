fff993d2ce8c5dafdb6b38547ef5c1537223b0f6
Phoenix <someshchavhan904@gmail.com>
2022-11-05 19:55:21 +0530

    added header and footer and bootstrap

---
+++ src/Component/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+     <footer className='bg-dark text-light h100'>
+        
+
+        <p className='text-center'> &copy;Todo list</p>
+        
+     </footer>   
+    </>
+  )
+}
+
+export default Footer
---
+++ src/Component/Header.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {Nav,Navbar} from 'react-bootstrap';
+
+
+const Header = () => {
+  return (
+    <>
+      <Navbar bg='dark' variant='dark' expand='md'>
+        <Navbar.Brand href='#'>
+          <img src='./logo.png' alt="..." width={40} height={40} />{'                    '}
+          Todo List
+        </Navbar.Brand>
+
+        <Navbar.Toggle/>
+        <Navbar.Collapse>
+        <Nav>
+          <Nav.Link href='#'>Home</Nav.Link>
+          <Nav.Link href='#about'>About</Nav.Link>
+        </Nav>
+
+        </Navbar.Collapse>
+        
+      </Navbar>
+    </>
+  )
+}
+
+export default Header;
