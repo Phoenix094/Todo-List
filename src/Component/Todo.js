895362f765a69ab6bb4bcf533cf965af0cc31f5c
Phoenix <someshchavhan904@gmail.com>
2022-11-06 15:29:27 +0530

    added todo item and todoItem

--- src/Component/Todo.js
+++ src/Component/Todo.js
@@ -1,23 +1,35 @@
 import React from "react";
 import "./Css/section.css";
-import { Button, Container, Form } from "react-bootstrap";
+import { Container, Form } from "react-bootstrap";
+import TodoItem from "./TodoItem";
 
 const Todo = () => {
+
+  const myStyle = {
+    border: 'white 2px',
+    borderStyle: 'solid'
+  }
+
+
   return (
     <>
-      <div className="section">
+      <div className="section text-light">
         <Container>
           <Form>
             <Form.Group className="pt-3 ">
               <h3 className="text-light"> Todo Title</h3>
               <Form.Control className="w-50 " type="text" />
-            </Form.Group><br/>
+            </Form.Group><br />
             <Form.Group className="pt-3 mb-4">
               <h3 className="text-light"> Todo Descrption</h3>
               <Form.Control className="w-50" type="text" />
             </Form.Group>
-            <Button variant="primary" className="myStyle" >Add Todo</Button>
+            <button type="button" className="btn text-light" style={myStyle}>Add todo</button>
           </Form>
+
+
+
+          <TodoItem/>
         </Container>
       </div>
     </>
