ea994163071c460e0ddf7d2c4adaf3896210b9e5
Phoenix <someshchavhan904@gmail.com>
2022-11-06 18:23:09 +0530

    commit

--- src/Component/Addtodo.js
+++ src/Component/Addtodo.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState}from 'react'
 import { Form } from "react-bootstrap";
 
 const Addtodo = () => {
@@ -7,17 +7,21 @@ const Addtodo = () => {
         borderStyle: 'solid'
       }
 
+      const [title, setTitle] = useState('');
+      const [desc, setDesc] = useState('');
+
+
 
   return (
     <>
         <Form>
             <Form.Group className="pt-3 ">
               <h3 className="text-light"> Todo Title</h3>
-              <Form.Control className="w-50 " type="text" />
+              <Form.Control className="w-50 " type="text" value={title} onChange={(e) => {setTitle(e.target.value)}}/>
             </Form.Group><br />
             <Form.Group className="pt-3 mb-4">
-              <h3 className="text-light"> Todo Descrption</h3>
-              <Form.Control className="w-50" type="text" />
+              <h3 className="text-light" > Todo Descrption</h3>
+              <Form.Control className="w-50" type="text" value={desc} onChange={(e) => {setDesc(e.target.value)}}/>
             </Form.Group>
             <button type="button" className="btn text-light" style={myStyle}>Add todo</button>
           </Form>
