aa7e7597c7bdd865d5dfa18d40aaefe13e6628f8
Phoenix <someshchavhan904@gmail.com>
2022-11-06 18:22:55 +0530

    Change the Todo.js merge the TodoItem and Todo

--- src/Component/Todo.js
+++ src/Component/Todo.js
@@ -1,18 +1,30 @@
 import React from "react";
 import "./Css/section.css";
-import { Container, Form } from "react-bootstrap";
-import TodoItem from "./TodoItem";
+
+
 
 const Todo = () => {
 
-  
-
-
   return (
     <>
-      <TodoItem/>
+      <TodoItem />
     </>
   );
 };
+
+const TodoItem = () => {
+
+  return (
+
+    <>
+      <div className='mt-4 border-bottom'>
+        <h3 >Goto College</h3>
+        <p>going college tommarow at 10.00 am</p>
+        <button type='button' className='btn btn-danger mb-2' > Delete</button>
+      </div>
+    </>
+  )
+}
+
 
 export default Todo;
